7f4edb6b3127a29d206a8ba7a43f602531124c6d
mismetti <milastrokes@gmail.com>
2022-06-29 17:39:06 -0300

    login xml created

--- app/src/main/java/com/miladev/vacationhome/autenticacao/LoginActivity.java
+++ app/src/main/java/com/miladev/vacationhome/autenticacao/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.miladev.vacationhome;
+package com.miladev.vacationhome.autenticacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.miladev.vacationhome.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
